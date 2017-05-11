caae939471d1daf6889fa40e836d692d88f4b7c2
smguo <symguo@gmail.com>
2017-05-11 16:10:57 -0400

    Added clustering analysis

--- synapse_clustering.py
+++ synapse_clustering.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 """
-This script performs t-SNE on synaptic features extracted from PRISM data. 
-This script loads the synaptic features from the MAT files, concatenates the 
-features from the same well or from the same replicate together (depending on 
-the value of "split_wells"), subsample the synapses if the sample size is too 
-large, standardize the features, and gereate t-SNE maps for the features. The 
-output t-SNE maps are color-coded based on the feature values. 
-  
+This script performs hierachical clustering using Seaborn "clustermap" function
+on synaptic features extracted from PRISM data. 
+The script loads the synaptic features from the MAT files, concatenates the features from 
+the same well or from the same replicate together (depending on the value of "split_wells"), 
+subsamples the synapses if the sample size is too large, standardizes the features, and 
+performs hierachical clustering. The clustering result was represented using dendrograms 
+and heatmaps.  
 Created on Sat Nov 19 20:04:01 2016
 @author: Syuan-Ming Guo
 """
@@ -17,16 +17,20 @@ import glob
 import matplotlib.pyplot as plt
 import scipy.io
 from time import time
-from matplotlib.ticker import NullFormatter
+import pandas as pd
+import seaborn as sns
 from sklearn import manifold
 import matplotlib as mpl
+import math
+import scipy
+#import pylab
+import scipy.cluster.hierarchy as sch
 
-
-mpl.rcParams['pdf.fonttype'] = 42 # change the default font settings of matplotlib
+mpl.rcParams['pdf.fonttype'] = 42 # change the default settings of matplotlib
 mpl.rcParams['ps.fonttype'] = 42
 mpl.rcParams['svg.fonttype'] = 'none'
 mpl.rcParams.update({'figure.autolayout': True})
-#sns.set_context("poster")
+sns.set_context("poster")
 #%%
 def read_features(dir_path): # load synaptic features from MAT files
     subdir_path = glob.glob(os.path.join(dir_path, '*/'))    
@@ -133,75 +137,74 @@ def left_outlier_1d(points, thresh=-3.5): # find outlier indices at only the lef
 
     return modified_z_score < thresh
     
-def build_features(IabInMarker, IabWithMarker, AabWithMarker,log_transform=False):# subsample the synapses if needed, stadardize the features and return the feature matrix
+def build_features(IabInMarker, IabWithMarker, AabWithMarker, log_transform=False): # subsample the synapses if needed, stadardize the features and return the feature matrix
     IabWithMarker[:,8] = IabInMarker[:,8]
     IabWithMarker[:,12] = IabInMarker[:,12]
-#
-    X = np.hstack((IabWithMarker, AabWithMarker[:,0:8], AabWithMarker[:,9:12])) # leave out the areas of Tuj-1 and MAP2    
-#    X = np.hstack((IabWithMarker[:,0:8], IabWithMarker[:,9:12], AabWithMarker[:,0:8], AabWithMarker[:,9:12]))
+#    X = np.hstack((IabWithMarker[:,0:8], IabWithMarker[:,9:12], AabWithMarker[:,0:8], AabWithMarker[:,9:12])) # leave out Tuj-1 and MAP2  
+    X = np.hstack((IabWithMarker, AabWithMarker[:,0:8], AabWithMarker[:,9:12])) # leave out Tuj-1 and MAP2
     if log_transform:
-        X = np.log(X+1)    
-    X = stand_feature(X)  # standardize the feature matrix
-    n_sub = 10000 #subsample size
-    prng = np.random.RandomState(1)
+        X = np.log(X+1)        
+    X = stand_feature(X)    
+    n_sub = 58000 #subsample size. No subsampling for clustering
+    
     if X.shape[0]>n_sub:
-        rand_arr = prng.rand(X.shape[0])
+        rand_arr = np.random.rand(X.shape[0])
         subsample_ind = rand_arr<(n_sub/X.shape[0])
         X = X[subsample_ind,:] # subsample the data to reduce memory usage  
 #        subsample_f = math.ceil(X.shape[0]/n_sub) # (subsample fraction)^-1
 #        X = X[0::subsample_f,:] # subsample the data to reduce memory usage     
     return X
     
-def run_tSNE(X, n_components = 2, perplexity = 100, n_iter = 5000): 
-    t0 = time()
-    tsne = manifold.TSNE(n_components=n_components, perplexity=perplexity, 
-                         n_iter= n_iter, method='exact',init='pca',
-                         random_state=1, verbose=2)
-    Y = tsne.fit_transform(X)
-    t1 = time()
-    print("t-SNE: %.2g sec" % (t1 - t0))
-    return Y    
-#%%    
-def plot_tSNE(X, imgRoundNamesNoWO,perplexity, plotInd): # plot the t-SNE map
-    label = imgRoundNamesNoWO # labels of targets
-    labelI = ["I(" + s + ")" for s in label]
+def run_clustering(IabInMarker, IabWithMarker, AabWithMarker,labelIA,log_transform=False):        
+    X = build_features(IabInMarker, IabWithMarker, AabWithMarker, log_transform=log_transform)         
+    X_df = pd.DataFrame(X.T, index=labelIA)        
+    g = sns.clustermap(X_df, method='ward', metric='euclidean', 
+       figsize=(4, 12),xticklabels=False, cmap = 'bwr',vmin=-6, vmax=6)
+    plt.setp(g.ax_heatmap.get_yticklabels(), rotation=0)                                                   
+    #            plot_clustergram(X, Y1, Y2, imgRoundNamesNoWO)
+    return g
+    
+def plot_clustergram(X, Y1, Y2, imgRoundNamesNoWO): # Alternative to sns.clustermap. Currently unused 
     label = np.hstack((imgRoundNamesNoWO[0:8], imgRoundNamesNoWO[9:12]))
+    labelI = ["I(" + s + ")" for s in label]
     labelA = ["A(" + s + ")" for s in label]
     labelIA = np.hstack((labelI, labelA))
-    labelIA = labelIA[plotInd]
-    cmapList = ['Blues', 'Greens', 'Oranges', 'RdPu', 'Purples', 'Reds']
     
-#    label = np.hstack((imgRoundNamesNoWO[0:8], imgRoundNamesNoWO[9:12]))
-#    labelI = ["I(" + s + ")" for s in label]
-#    labelA = ["A(" + s + ")" for s in label]
-#    labelIA = np.hstack((labelI, labelA))
-    #% scan colormaps
-    Ind_outlier = is_outlier(Y, thresh=1.5)
-#    fig = plt.figure(figsize=(20, 12))
-    fig = plt.figure(figsize=(10, 6))
-    plt.suptitle("t-SNE with p= %i color-coded by each feature"
-                 % (perplexity), fontsize=14)
-    for j in range(0, len(plotInd)):        
-        color = X[~Ind_outlier,plotInd[j]]
-#        ax = fig.add_subplot(4, 6, j+1)
-        ax = fig.add_subplot(2, 3, j+1)
-        sc = plt.scatter(Y[~Ind_outlier, 0], Y[~Ind_outlier, 1], c=color, s=1,cmap=plt.get_cmap(cmapList[j]),
-                         vmin=0, vmax=6, edgecolors='none')
-        plt.title("%s" % (labelIA[j])) 
-        plt.colorbar(sc)
-        ax.xaxis.set_major_formatter(NullFormatter())
-        ax.yaxis.set_major_formatter(NullFormatter())
-        ax.set_axis_bgcolor((0.7,0.7,0.7))
-        plt.axis('tight')    
-        plt.show()           
+    fig = pylab.figure(figsize=(8,8))
+    ax1 = fig.add_axes([0.09,0.1,0.2,0.6])    
+    Z1 = sch.dendrogram(Y1)
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    
+    # Compute and plot second dendrogram.
+    ax2 = fig.add_axes([0.3,0.71,0.6,0.2])    
+    Z2 = sch.dendrogram(Y2,orientation='right')
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    
+    # Plot distance matrix.
+    axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
+    idx1 = Z1['leaves']
+    idx2 = Z2['leaves']
+    X = X[idx1,:]
+    X = X[:,idx2]
+    im = axmatrix.matshow(X, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu)
+    axmatrix.set_xticks([])
+    axmatrix.set_yticks([])
+    
+    # Plot colorbar.
+    axcolor = fig.add_axes([0.91,0.1,0.02,0.6])
+    pylab.colorbar(im, cax=axcolor)
+    fig.show()
+    fig.savefig('dendrogram.png')
+     
 #%%
-fig_path = 'E:/Google Drive/Python/figures/'  # change 'fig_path' to the desired figure output path. 
-plotInd = [4,6,0,1,2,3] # features that are used to color code 
-for rep_num in range(1, 4): # specify replicate number    
+fig_path = 'E:/Google Drive/Python/figures/'   # change 'fig_path' to the desired figure output path. 
+for rep_num in range(1, 2): # specify replicate number    
     split_wells = False # if "True", run t-SNE for each well in the replicate, otherwise pool data from all the wells together
     
     if rep_num == 1:
-        dir_path = "E:/data/Neuron/cortical/Broad_HCS/14days/20161021-crotalk in cells-t9-Rep2/post_processing_multicolor/"  # change the directory path for the MAT file       
+        dir_path = "E:/data/Neuron/cortical/Broad_HCS/14days/20161021-crotalk in cells-t9-Rep2/post_processing_multicolor/"     # Change `dir_path' in the script to the folder where the MAT files are located    
     elif rep_num == 2:        
         dir_path = "E:/data/Neuron/cortical/Broad_HCS/14days/20161027-Rep3 and Rep4/Rep3/post_processing_multicolor/"         
     elif rep_num == 3:        
@@ -211,80 +214,35 @@ for rep_num in range(1, 4): # specify replicate number
         
     IabInMarkerAll, IabWithMarkerAll, AabWithMarkerAll, imgRoundNamesNoWO = read_features(dir_path)
     nWell = IabInMarkerAll.shape[0]
-    #%
-    n_components = 2 # set the t-SNE parameters
-    perplexity = 40
-    n_iter = 5000
-    
+    #%%    
+    label = imgRoundNamesNoWO # labels of targets
+    labelI = ["I(" + s + ")" for s in label]
+    label = np.hstack((imgRoundNamesNoWO[0:8], imgRoundNamesNoWO[9:12]))
+    labelA = ["A(" + s + ")" for s in label]
+    labelIA = np.hstack((labelI, labelA))
     if split_wells:    
-        for j in range(0, nWell): # going through each well
+        for j in range(0, nWell): # going through each sub-folder
             IabInMarker = IabInMarkerAll[j] 
             IabWithMarker = IabWithMarkerAll[j] 
             AabWithMarker = AabWithMarkerAll[j]
-            X = build_features(IabInMarker, IabWithMarker, AabWithMarker)
-            Y = run_tSNE(X, n_components, perplexity, n_iter)
-            plot_tSNE(X, imgRoundNamesNoWO,perplexity)
-            plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_no_Tuj1_MAP2_p= %i_rep%i_well%i.png'% (perplexity, rep_num, j+1),dpi=300)
+            g = run_clustering(IabInMarker, IabWithMarker, AabWithMarker,labelIA)
+            title = 'rep%i-%i'% (rep_num, j+1)
+            plt.title(title)
+            plt.tight_layout()
+            plt.savefig(fig_path+'cluster_Iabwith+Aab_'+title+'.png',dpi=300)
+            plt.savefig(fig_path+'cluster_Iabwith+Aab_'+title+'.pdf',dpi=300)                            
     #        plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_no_Tuj1_MAP2_p= %i_rep%i_well%i.eps'% (perplexity, rep_num, j),dpi=300)
             plt.close("all")
     else:        
         IabInMarker, IabWithMarker, AabWithMarker = concat_data(IabInMarkerAll, IabWithMarkerAll, AabWithMarkerAll)
-        X = build_features(IabInMarker, IabWithMarker, AabWithMarker, log_transform =True)
-        Y = run_tSNE(X, n_components, perplexity, n_iter)
-        plot_tSNE(X, imgRoundNamesNoWO,perplexity,plotInd)
-#        plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_new_log_exact_p= %i_rep%i.png'% (perplexity, rep_num),dpi=300)
-        plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_new_log_exact_sub_p= %i_rep%i.png'% (perplexity, rep_num),dpi=300)
-#        plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_new_log_exact_sub_p= %i_rep%i.eps'% (perplexity, rep_num),dpi=300)
-        plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_new_log_exact_sub_p= %i_rep%i.pdf'% (perplexity, rep_num),dpi=300)
-#    plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_no_Tuj1_MAP2_p= %i_rep%i.eps'% (perplexity, rep_num),dpi=300)
+        g = run_clustering(IabInMarker, IabWithMarker, AabWithMarker,labelIA,log_transform=False)
+        title = 'rep%i_narrow'% (rep_num)
+        plt.title(title)
+        plt.tight_layout()
+        plt.savefig(fig_path+'cluster_Iabwith+Aab_'+title+'.png',dpi=300)
+        plt.savefig(fig_path+'cluster_Iabwith+Aab_'+title+'.pdf',dpi=300)       
+#        plt.savefig('E:/Google Drive/Python/figures/t-SNE_Iabwith+Aab_no_Tuj1_MAP2_p= %i_rep%i_well%i.eps'% (perplexity, rep_num, j),dpi=300)        
         plt.close("all")
-#    
+        
 
-
-#%% scan perplexity values
-#n_neighbors = 10
-#n_components = 2
-##perplexity = [10, 40, 100, 400, 1000, 4000]
-##perplexity = [10, 40, 100]
-#perplexity = [100]
-#n_iter = [5000]
-#color = X[:,0]
-##%
-#fig = plt.figure(figsize=(20, 8))
-#plt.suptitle("t-SNE_perplexity_scan", fontsize=14)
-#
-#for k in range(0, len(perplexity)): 
-##for k in range(0, len(n_iter)): 
-#    
-#
-#    ax = fig.add_subplot(1, 3, k+1)
-#    sc = plt.scatter(Y[:, 0], Y[:, 1], c=color, s=5,cmap=plt.cm.nipy_spectral, edgecolors='none')
-#    plt.title("p= %i (%.2g sec)" % (perplexity[k],t1 - t0))
-##    plt.title("n_iter= %i (%.2g sec)" % (n_iter[k],t1 - t0))
-#    plt.colorbar(sc)
-#    ax.xaxis.set_major_formatter(NullFormatter())
-#    ax.yaxis.set_major_formatter(NullFormatter())
-#    plt.axis('tight')    
-#    plt.show()
-##    plt.savefig('E:/Google Drive/Python/figures/t-SNE_perplexity_scan_no_Tuj1.png',dpi=300)
-##    plt.savefig('E:/Google Drive/Python/figures/t-SNE_niter_scan_no_Tuj1_exact.png',dpi=300)
-#    
-##%% scan colormaps
-#    Ind_outlier = is_outlier(Y, thresh=2)
-#    fig = plt.figure(figsize=(20, 12))
-#    plt.suptitle("t-SNE with p= %i color-coded by each feature"
-#                 % (perplexity[k]), fontsize=14)
-#    for j in range(0, X.shape[1]):        
-#        color = X[~Ind_outlier,j]
-#        ax = fig.add_subplot(4, 6, j+1)
-#        sc = plt.scatter(Y[~Ind_outlier, 0], Y[~Ind_outlier, 1], c=color, s=1,cmap=plt.cm.nipy_spectral,
-#                         vmin=0, vmax=6, edgecolors='none')
-#        plt.title("%s" % (labelIA[j])) 
-#        plt.colorbar(sc)
-#        ax.xaxis.set_major_formatter(NullFormatter())
-#        ax.yaxis.set_major_formatter(NullFormatter())
-#        plt.axis('tight')    
-#        plt.show()
-#    plt.savefig('E:/Google Drive/Python/figures/t-SNE_color_coded_feature_Iabwith+Aab_no_Tuj1_MAP2_p= %i_rep%i.png'% (perplexity[k], rep_num),dpi=300)
-#    plt.savefig('E:/Google Drive/Python/figures/t-SNE_color_coded_feature_Iabwith+Aab_no_Tuj1_MAP2_p= %i_rep%i.eps'% (perplexity[k], rep_num),dpi=300)
-#    plt.savefig('E:/Google Drive/Python/figures/t-SNE_color_coded_feature_Iabwith+Aab_no_Tuj1_MAP2_p= %i_rep%i.svg'% (perplexity[k], rep_num),dpi=300)
+    
